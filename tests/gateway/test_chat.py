0ed653b08bc192218c265d94438aa5b7ab6a9f24
Imran Qureshi <imranq2@hotmail.com>
2024-11-15 11:33:53 -0800

    add sample tesdt

--- tests/gateway/test_chat.py
+++ tests/gateway/test_chat.py
@@ -1,14 +1,31 @@
+from typing import Generator
+
+import httpx
 import pytest
 from openai import OpenAI
 
+from language_model_gateway.gateway.api import app
+
+
+@pytest.fixture
+def httpx_client() -> Generator[httpx.Client, None, None]:
+    with httpx.Client(app=app, base_url="http://localhost") as client:
+        yield client
+
 
 @pytest.mark.asyncio
-async def test_chat_completions() -> None:
+async def test_chat_completions(httpx_client: httpx.Client) -> None:
+
+    # get /health url
+    response = httpx_client.get("/health")
+    assert response.status_code == 200
+    assert response.text == "OK"
 
     # init client and connect to localhost server
     client = OpenAI(
         api_key="fake-api-key",
         base_url="http://localhost:5000",  # change the default port if needed
+        http_client=httpx_client,
     )
 
     # call API
