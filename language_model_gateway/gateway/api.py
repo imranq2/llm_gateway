cdb91c44fc7d0613874cc79abdebc9e4aa33b566
Imran Qureshi <imranq2@hotmail.com>
2024-11-15 14:02:22 -0800

    get openweb-ui to work

--- language_model_gateway/gateway/api.py
+++ language_model_gateway/gateway/api.py
@@ -3,7 +3,7 @@ import json
 import logging
 import random
 import time
-from typing import Dict, Any, AsyncGenerator, cast
+from typing import Dict, AsyncGenerator, cast
 from typing import List
 
 from fastapi import FastAPI
@@ -33,25 +33,6 @@ def health() -> str:
     return "OK"
 
 
-@app.post("/api/v1/chat/completions_sync")
-async def chat_completions_sync(request: ChatRequest) -> Dict[str, Any]:
-    resp_content: str
-    if request.messages and request.messages[0].role == "user":
-        resp_content = cast(str, cast(UserMessage, request.messages[-1]).content)
-    else:
-        resp_content = "As a mock AI Assistant, I can only echo your last message, but there were no messages!"
-
-    return {
-        "id": "1337",
-        "object": "chat.completion",
-        "created": time.time(),
-        "model": request.model,
-        "choices": [
-            {"message": ChatResponseMessage(role="assistant", content=resp_content)}
-        ],
-    }
-
-
 async def _resp_async_generator(text_resp: str) -> AsyncGenerator[str, None]:
     # let's pretend every word is a token and return it over time
     tokens = text_resp.split(" ")
@@ -59,7 +40,6 @@ async def _resp_async_generator(text_resp: str) -> AsyncGenerator[str, None]:
     for i, token in enumerate(tokens):
         chunk: ChatStreamResponse = ChatStreamResponse(
             id=str(i),
-            object="chat.completion.chunk",
             created=int(time.time()),
             model="blah",
             choices=[
@@ -90,12 +70,11 @@ async def chat_completions(
     if request.stream:
         logger.info(f"Streaming response {request_id}")
         return StreamingResponse(
-            _resp_async_generator(resp_content), media_type="application/x-ndjson"
+            _resp_async_generator(resp_content), media_type="text/event-stream"
         )
 
     response_dict: ChatResponse = ChatResponse(
         id="1337",
-        object="chat.completion",
         created=int(time.time()),
         model=request.model,
         choices=[
