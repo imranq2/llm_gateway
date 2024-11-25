5d0ae955c167690aa78c023c3a8bb99085fadd48
Imran Qureshi <imranq2@hotmail.com>
2024-11-25 15:14:19 -0800

    implement streaming

--- language_model_gateway/gateway/providers/openai_chat_completions_provider.py
+++ language_model_gateway/gateway/providers/openai_chat_completions_provider.py
@@ -2,9 +2,10 @@ import json
 import logging
 from os import environ
 from random import randint
-from typing import Any, Dict
+from typing import Any, Dict, AsyncGenerator
 
 from httpx import Response
+from httpx_sse import aconnect_sse, ServerSentEvent
 from openai.types.chat import (
     ChatCompletion,
 )
@@ -37,9 +38,21 @@ class OpenAiChatCompletionsProvider(BaseChatCompletionsProvider):
         """
         assert chat_request
 
-        request_id = randint(1, 1000)
+        request_id: str = str(randint(1, 1000))
         agent_url: Optional[str] = environ["OPENAI_AGENT_URL"]
         assert agent_url
+
+        if chat_request.get("stream"):
+            return StreamingResponse(
+                await self.get_streaming_response_async(
+                    agent_url=agent_url,
+                    request_id=request_id,
+                    headers=headers,
+                    chat_request=chat_request,
+                ),
+                media_type="text/event-stream",
+            )
+
         async with httpx.AsyncClient(base_url=agent_url) as client:
             try:
                 agent_response: Response = await client.post(
@@ -59,3 +72,56 @@ class OpenAiChatCompletionsProvider(BaseChatCompletionsProvider):
             response: ChatCompletion = ChatCompletion.parse_obj(response_dict)
             logger.info(f"Non-streaming response {request_id}: {response}")
             return JSONResponse(content=response.model_dump())
+
+    async def get_streaming_response_async(
+        self,
+        *,
+        agent_url: str,
+        request_id: str,
+        headers: Dict[str, str],
+        chat_request: ChatRequest,
+    ) -> AsyncGenerator[str, None]:
+        logger.info(f"Streaming response {request_id} from agent")
+        generator: AsyncGenerator[str, None] = self._stream_resp_async_generator(
+            agent_url=agent_url,
+            request_id=request_id,
+            chat_request=chat_request,
+            headers=headers,
+        )
+        return generator
+
+    @staticmethod
+    async def _stream_resp_async_generator(
+        *,
+        request_id: str,
+        agent_url: str,
+        chat_request: ChatRequest,
+        headers: Dict[str, str],
+    ) -> AsyncGenerator[str, None]:
+
+        logger.info(f"Streaming response {request_id} from agent")
+        async with httpx.AsyncClient(base_url=agent_url) as client:
+            async with aconnect_sse(
+                client,
+                "POST",
+                "/chat/completions",
+                json=chat_request,
+                timeout=60 * 60,
+                headers=headers,
+            ) as event_source:
+                i = 0
+                sse: ServerSentEvent
+                async for sse in event_source.aiter_sse():
+                    event: str = sse.event
+                    data: str = sse.data
+                    i += 1
+
+                    if logger.isEnabledFor(logging.DEBUG):
+                        logger.debug(
+                            f"----- Received data from stream {i} {event} {type(data)} ------"
+                        )
+                        logger.debug(data)
+                        logger.debug(
+                            f"----- End data from stream {i} {event} {type(data)} ------"
+                        )
+                    yield f"data: {data}\n\n"
