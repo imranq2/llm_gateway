12ee85ab9024624b918f929e34d64390059501f0
Imran Qureshi <imranq2@hotmail.com>
2024-11-27 18:06:22 -0800

    add google search tool

--- language_model_gateway/gateway/tools/google_search_tool.py
+++ language_model_gateway/gateway/tools/google_search_tool.py
@@ -1,18 +1,37 @@
-from typing import Any, cast
+import logging
+from os import environ
+from typing import cast
 
-from langchain_google_community import GoogleSearchAPIWrapper
 from langchain_core.tools import BaseTool
+from langchain_google_community import GoogleSearchAPIWrapper
+
+logger = logging.getLogger(__file__)
 
 
 class GoogleSearchTool(BaseTool):
     name: str = "google_search"
     description: str = "Search Google for recent results."
 
-    def _run(self, *args: Any, **kwargs: Any) -> str:
+    def _run(self, query: str) -> str:
         """Returns the current time in Y-m-d H:M:S format with timezone."""
-        search = GoogleSearchAPIWrapper()
-        return cast(str, search.run(*args, **kwargs))
+        try:
+            search = GoogleSearchAPIWrapper()
+            logger.info(f"Running Google search with query: {query}")
+            google_api_key = environ["GOOGLE_API_KEY"]
+            assert (
+                google_api_key is not None
+            ), "You need to specify a GOOGLE_API_KEY in docker.env to run the Google Search tool"
+            google_cse_id = environ["GOOGLE_CSE_ID"]
+            assert (
+                google_cse_id is not None
+            ), "You need to specify a GOOGLE_CSE_ID in docker.env to run the Google Search tool"
+            result: str = cast(str, search.run(query=query))
+            logger.info(f"Google search result: {result}")
+            return result
+        except Exception as e:
+            logger.error(f"Error running Google search: {e}")
+            return f"Error running Google search: {e}"
 
-    async def _arun(self, *args: Any, **kwargs: Any) -> str:
+    async def _arun(self, query: str) -> str:
         """Async implementation of the tool (in this case, just calls _run)"""
-        return self._run(*args, **kwargs)
+        return self._run(query=query)
