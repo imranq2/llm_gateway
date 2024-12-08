0e8ed24b08c228f4ff38e5b7a9c0d9c076bf179b
Imran Qureshi <imranq2@hotmail.com>
2024-12-08 13:31:24 -0800

    add reader to read from github

--- language_model_gateway/configs/config_reader/config_reader.py
+++ language_model_gateway/configs/config_reader/config_reader.py
@@ -17,16 +17,21 @@ class ConfigReader:
         config_path: str = os.environ["CONFIG_PATH"]
         assert config_path is not None, "CONFIG_PATH environment variable is not set"
 
-        models: List[ChatModelConfig] = []
+        models: List[ChatModelConfig]
         if config_path.startswith("s3"):
             models = S3ConfigReader().read_model_configs(s3_url=config_path)
         elif "github.com" in config_path:
             models = await GitHubConfigReader(
                 os.environ.get("GITHUB_TOKEN")
             ).read_model_configs(github_url=config_path)
+        else:
+            models = FileConfigReader().read_model_configs(config_path=config_path)
 
         # if we can't load models another way then try to load them from the file system
         if not models or len(models) == 0:
-            models = FileConfigReader().read_model_configs(config_path=config_path)
+            config_path_backup: str = os.environ["CONFIG_PATH_BACKUP"]
+            models = FileConfigReader().read_model_configs(
+                config_path=config_path_backup
+            )
 
         return models
