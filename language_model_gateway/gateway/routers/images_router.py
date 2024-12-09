3c53c669d59e3ff9158fb758b03507b6ea4257e8
Imran Qureshi <imranq2@hotmail.com>
2024-12-09 13:24:57 -0800

    get image download to work

--- language_model_gateway/gateway/routers/images_router.py
+++ language_model_gateway/gateway/routers/images_router.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from enum import Enum
-from typing import Annotated, Dict, List, Sequence
+from typing import Annotated, List, Sequence
 
 from fastapi import APIRouter, Depends
 from fastapi import params
@@ -45,10 +45,10 @@ class ImagesRouter:
     def _register_routes(self) -> None:
         """Register all routes for this router"""
         self.router.add_api_route(
-            "/image_generation",
+            "/image_generation/{file_path:path}",  # Add path parameter to capture the full path
             self.get_images,
             methods=["GET"],
-            response_model=Dict[str, List[Dict[str, str]]],
+            response_model=None,
             summary="Download images",
             description="Download images",
             response_description="Images",
@@ -66,6 +66,7 @@ class ImagesRouter:
     async def get_images(
         self,
         request: Request,
+        file_path: str,  # Add this parameter to capture the file path
         file_manager_factory: Annotated[
             FileManagerFactory, Depends(get_file_manager_factory)
         ],
@@ -76,34 +77,36 @@ class ImagesRouter:
         Args:
             request: The incoming request
             file_manager_factory: Injected model manager instance
+            file_path: The file path to download
 
         Returns:
             Dictionary containing list of available models
         """
-        request_url_path = request.url.path
+        request_url_path = file_path
+        logger.info(f"get_images: request.url.path: {file_path}")
         folder: str
-        file_path: str
+        file_path1: str
         if self.image_generation_path.startswith("s3"):
             bucket_name, prefix = UrlParser.parse_s3_uri(self.image_generation_path)
             # Check file extension
-            file_path = str(request_url_path)
+            file_path1 = str(request_url_path)
             # remove the target path
-            file_path = file_path[len(self.prefix) :]
+            file_path1 = file_path1[len(self.prefix) :]
 
-            if not self.check_extension(file_path):
+            if not self.check_extension(file_path1):
                 return Response(status_code=403, content="File type not allowed")
 
             # combine the prefix and file path and include / if needed
-            s3_key = os.path.join(prefix.rstrip("/"), file_path.lstrip("/")).replace(
+            s3_key = os.path.join(prefix.rstrip("/"), file_path1.lstrip("/")).replace(
                 "\\", "/"
             )
             folder = bucket_name
-            file_path = s3_key
+            file_path1 = s3_key
         else:
             # read and return file
             request_url_path = request_url_path[len(self.prefix) :]
             folder = self.image_generation_path
-            file_path = request_url_path
+            file_path1 = request_url_path
 
         # now stream the file
         file_manager: FileManager = file_manager_factory.get_file_manager(
@@ -111,7 +114,7 @@ class ImagesRouter:
         )
         return await file_manager.read_file_async(
             folder=folder,
-            file_path=file_path,
+            file_path=file_path1,
         )
 
     def get_router(self) -> APIRouter:
