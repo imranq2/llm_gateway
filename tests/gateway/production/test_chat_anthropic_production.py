dfbdb60573f9b7a002de1694fd0a6c649278d12d
Imran Qureshi <imranq2@hotmail.com>
2024-12-18 19:36:00 -0800

    add streaming test

--- tests/gateway/production/test_chat_anthropic_production.py
+++ tests/gateway/production/test_chat_anthropic_production.py
@@ -3,8 +3,8 @@ from typing import Optional
 
 import httpx
 import pytest
-from openai import AsyncOpenAI
-from openai.types.chat import ChatCompletion
+from openai import AsyncOpenAI, AsyncStream
+from openai.types.chat import ChatCompletion, ChatCompletionChunk
 
 
 @pytest.mark.skipif(
@@ -17,7 +17,7 @@ async def test_chat_completions_production(async_client: httpx.AsyncClient) -> N
     # init client and connect to localhost server
     client = AsyncOpenAI(
         api_key="fake-api-key",
-        base_url="https://language-model-gateway.dev.bwell.zone/api/v1",
+        base_url="https://language-model-gateway.services.bwell.zone/api/v1",
         http_client=async_client,
     )
 
@@ -36,6 +36,111 @@ async def test_chat_completions_production(async_client: httpx.AsyncClient) -> N
     content: Optional[str] = "\n".join(
         choice.message.content or "" for choice in chat_completion.choices
     )
+
+    assert content is not None
+    print(content)
+    # assert "Barack" in content
+
+
+# @pytest.mark.skipif(
+#     os.getenv("RUN_TESTS_WITH_REAL_LLM") != "1",
+#     reason="hits production API",
+# )
+async def test_chat_completions_json_output_production(
+    async_client: httpx.AsyncClient,
+) -> None:
+    print("")
+
+    # init client and connect to localhost server
+    client = AsyncOpenAI(
+        api_key="fake-api-key",
+        base_url="https://language-model-gateway.services.bwell.zone/api/v1",
+        http_client=async_client,
+    )
+
+    # call API
+    chat_completion: ChatCompletion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": """Get the address of Vanessa Paz NP at One Medical.    
+                Return the output in JSON format using the following schema: 
+                {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "doctor_name": {
+      "type": "string",
+      "description": "The full name of the doctor"
+    },
+    "doctor_address": {
+      "type": "string", 
+      "description": "The complete address of the doctor's practice or office"
+    },
+    "doctor_phone": {
+      "type": "string",
+      "description": "The contact phone number for the doctor"
+    }
+  },
+  "required": ["doctor_name", "doctor_address", "doctor_phone"]
+}
+                """,
+            }
+        ],
+        model="General Purpose",
+    )
+
+    # print the top "choice"
+    content: Optional[str] = "\n".join(
+        choice.message.content or "" for choice in chat_completion.choices
+    )
+
+    assert content is not None
+    print("======= JSON Output =======")
+    print(content)
+    print("======= End of JSON Output =======")
+    # assert "Barack" in content
+
+
+@pytest.mark.skipif(
+    os.getenv("RUN_TESTS_WITH_REAL_LLM") != "1",
+    reason="hits production API",
+)
+async def test_chat_completions_streaming_production(
+    async_client: httpx.AsyncClient,
+) -> None:
+    print("")
+
+    # init client and connect to localhost server
+    client = AsyncOpenAI(
+        api_key="fake-api-key",
+        base_url="https://language-model-gateway.services.bwell.zone/api/v1",
+        http_client=async_client,
+    )
+
+    # call API
+    stream: AsyncStream[ChatCompletionChunk] = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Get the address of Dr. Meggin A. Sabatino at Medstar",
+            }
+        ],
+        model="General Purpose",
+        stream=True,
+    )
+
+    # print the top "choice"
+    content: Optional[str] = None
+    chunk: ChatCompletionChunk
+    async for chunk in stream:
+        delta_content = "\n".join(
+            [choice.delta.content or "" for choice in chunk.choices]
+        )
+        print("======= Chunk =======")
+        print(delta_content)
+        print("======= End of Chunk =======")
+        content = content + delta_content if content else delta_content
 
     assert content is not None
     print(content)
