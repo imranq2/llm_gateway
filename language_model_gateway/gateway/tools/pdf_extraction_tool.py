65e6e1c09a159c272da1e110020cabc08cd8cc6c
Imran Qureshi <imranq2@hotmail.com>
2024-12-26 14:38:01 -0800

    add headers

--- language_model_gateway/gateway/tools/pdf_extraction_tool.py
+++ language_model_gateway/gateway/tools/pdf_extraction_tool.py
@@ -106,7 +106,7 @@ class PDFExtractionTool(BaseTool):
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
                     "Accept": "application/pdf, text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
                     "Accept-Language": "en-US,en;q=0.9",
-                    "Referer": "/".join(url.split("/")[:3]),  # Add base URL as referer
+                    # "Referer": "/".join(url.split("/")[:3]),  # Add base URL as referer
                 }
             )
             try:
